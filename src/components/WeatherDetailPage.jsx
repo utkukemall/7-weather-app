188b0076929c4222b1716fe2c75a5ecb5cacbb65
utkukemall <utkukemal1999@gmail.com>
2024-04-15 14:45:00 +0300

    Initial commit

---
+++ src/components/WeatherDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WeatherDetailPage = () => {
+    return (
+        <div>
+            <h1>Weather Detail Page</h1>
+            {/* Weather detail page content */}
+        </div>
+    );
+};
+
+export default WeatherDetailPage;
